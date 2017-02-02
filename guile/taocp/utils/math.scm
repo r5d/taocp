2132a92f04f6bb4bd86a2c4fc87e6a690f0d774c
rsiddharth <s@ricketyspace.net>
2017-02-02 03:25:29 +0000

    Update function `fib`' docstring.
    * guile/taocp/utils/math.scm (fib): Update docstring.

--- guile/taocp/utils/math.scm
+++ guile/taocp/utils/math.scm
@@ -19,7 +19,11 @@ For instance `(lambda (x) (* x x))` is a valid FX.
         (else (cons (fx n) (compute-exp1-n fx (1- n))))))
 
 (define (fib n)
-  "Returns Fibonacci of N."
+  "Returns Fibonacci of N.
+
+This function is useless for large values of N. Use `fib-fast`
+function defined in (taocp one two eight fib).
+"
   (cond ((= n 0) 0)
         ((= n 1) 1)
         (else (+ (fib (- n 1)) (fib (- n 2))))))
